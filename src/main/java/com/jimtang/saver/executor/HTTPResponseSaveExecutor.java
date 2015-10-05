ece3b861c99e7423da87d85ba422da9182b6173f
wxmann <wxfreakk91@gmail.com>
2015-10-04 17:24:34 -0700

    Replace try-catch-finally with try-with-resources block

--- src/main/java/com/jimtang/saver/executor/HTTPResponseSaveExecutor.java
+++ src/main/java/com/jimtang/saver/executor/HTTPResponseSaveExecutor.java
@@ -21,21 +21,9 @@ public abstract class HTTPResponseSaveExecutor implements SaveExecutor {
 
     protected abstract URI getURI();
 
-    private void closeResources(Closeable closeable) {
-        if (closeable != null) {
-            try {
-                closeable.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
     public final void doSave(String saveLocation) {
-        CloseableHttpResponse response = null;
-        try {
-            HttpGet httpGet = new HttpGet(getURI());
-            response = HTTP_CLIENT.execute(httpGet);
+        HttpGet httpGet = new HttpGet(getURI());
+        try (CloseableHttpResponse response = HTTP_CLIENT.execute(httpGet)){
             int code = response.getStatusLine().getStatusCode();
 
             if (code != 200) {
@@ -47,8 +35,6 @@ public abstract class HTTPResponseSaveExecutor implements SaveExecutor {
 
         } catch (IOException e) {
             throw new ImageRetrievalException(e);
-        } finally {
-            closeResources(response);
         }
     }
 }
