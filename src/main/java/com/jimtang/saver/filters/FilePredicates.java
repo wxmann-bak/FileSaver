8edec325eae37a17007be1d13521151b7703cf66
wxmann <wxfreakk91@gmail.com>
2016-01-25 01:41:48 -0800

    Cleanup file filtering module. Also missed check-in from last commit (Mockito dependency in POM).

--- src/main/java/com/jimtang/saver/filters/FilePredicates.java
+++ src/main/java/com/jimtang/saver/filters/FilePredicates.java
@@ -1,5 +1,8 @@
 package com.jimtang.saver.filters;
 
+import com.google.common.base.Strings;
+import com.google.common.io.Files;
+
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
@@ -12,11 +15,19 @@ public final class FilePredicates {
     }
 
     public static Predicate<String> forIsFile() {
-        return new IsFilePredicate();
+        return fileStr -> !Strings.isNullOrEmpty(Files.getFileExtension(fileStr));
     }
 
     public static Predicate<String> forAllowedFileTypes(String... fileTypes) {
-        return new FileTypePredicate(fileTypes);
+        return fileString -> {
+            String ext = Files.getFileExtension(fileString);
+            for (String fileExtension: fileTypes) {
+                if (ext.equalsIgnoreCase(fileExtension)) {
+                    return true;
+                }
+            }
+            return false;
+        };
     }
 
     public static Predicate<String> forContainingText(final String textSequence) {
